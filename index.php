bffbca7d28fe0a81b1b85cdf19461fe1f281b4a3
DavidW <djrival7@gmail.com>
2014-04-05 20:30:50 -0400

    Initialized Project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ProjectCurrently</title>
+ 
+  <%= javascript_include_tag "bootstrap", "data-turbolinks-track" => true %>
+  <%= javascript_include_tag "application", "data-turbolinks-track" => true %>
+  <%= stylesheet_link_tag "application.css", media: "all", "data-turbolinks-track" => true %>
+  <%= csrf_meta_tags %>
+</head>
+<body>
+
+	<%= render :partial => "layouts/nav" %>
+
+	<div class="container">
+	  <%= yield %>
+	</div>
+
+	<%= render :partial => "layouts/footer" %>
+
+</body>
+</html>
