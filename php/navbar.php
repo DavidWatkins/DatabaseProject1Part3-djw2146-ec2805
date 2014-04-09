8981d837d74e57d8cbe1a7ad36951b08afb8e083
DavidW <djrival7@gmail.com>
2014-04-08 21:03:03 -0400

    New Versions of index and navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
+=======
 <?php
-include_once 'php/includes/db_connect.php';
-include_once 'php/includes/functions.php';
+//include_once 'php/includes/db_connect.php';
+//include_once 'php/includes/functions.php';
 ?>
 
+>>>>>>> 391de013534262f237c5aca0a8812693f08e6c6e
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
@@ -17,13 +20,14 @@ include_once 'php/includes/functions.php';
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Projects</a></li>
-                <?php if (login_check($mysqli) == true) : ?>
+<<<<<<< HEAD
+=======
+                <?php if /*(login_check($mysqli)*/ true == true) : ?>
+>>>>>>> 391de013534262f237c5aca0a8812693f08e6c6e
                     <li class="active"><a href="profile.php?user_id">Dashboard</a></li>
                     <li class="active"><a href="/logout">Logout</a></li>
-                <?php else : ?>
                     <li class="active"><a href="/login.php">Login</a></li>
                     <li class="active"><a href="/register.php">Sign Up</a></li>
-                <?php endif; ?>
             </ul>
         </div>
     </div>
