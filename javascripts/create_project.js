8db6a49241fbaae5399c9cb07512f125f72862c6
DavidW <djrival7@gmail.com>
2014-04-10 06:29:25 -0400

    Create_Project is fully functional

--- javascripts/create_project.js
+++ javascripts/create_project.js
@@ -1,123 +1,123 @@
-           var link_count=-1;
-            $(document).ready(function () {
-                $('#addPublicityLink').on('click', function (e) {
+var link_count=-1;
+$(document).ready(function () {
+    $('#addPublicityLink').on('click', function (e) {
 
-                    link_count++;
-                    var form = "<div id=publicity_link" + link_count + ">\
+        link_count++;
+        var form = "<div id=publicity_link" + link_count + ">\
 <h5>New Publicity Link</h5>\
 <p>\
-<input type=\"text\" name='PLName'" + link_count.toString() + "' id='PLName'" + link_count.toString() + "' placeholder='Website Name' />\
-        </p>\
+<input type=\"text\" name='PLName" + link_count + "' id='PLName" + link_count + "' placeholder='Website Name' />\
+</p>\
 <p>\
-<input type=\"text\" name='PLURL'" + link_count.toString() + "' id='PLName'" + link_count.toString() + "'  placeholder='Website URL' />\
-        </p>\
-        </div>";
-                    $(this).parent().before(form);
-                    e.preventDefault();
+<input type=\"text\" name='PLURL" + link_count + "' id='PLName" + link_count + "'  placeholder='Website URL' />\
+</p>\
+</div>";
+        $(this).parent().before(form);
+        e.preventDefault();
 
-                });
-            });
+    });
+});
 
-            $(document).ready(function () {
-                $('#removePublicityLink').on('click', function (e) {
-                    if(link_count >= 0) {
-                        $("#publicity_link" + link_count.toString()).empty();
-                        $("#publicity_link"+link_count).remove();
-                        link_count--;
-                    }
-                });
-            });
-
-
-            function lengthTest(type, okay, id, max_length, index) {
-                length = 0;
-                if(index >= 0) {
-                    length = $(id + index).val().length;
-                } else {
-                    length = $(id).val().length;
-                }
-
-                if((length > max_length) || (length <= 0)) {
-                    $("#output").append(type + " length cannot exceed " + length + " characters<br />");
-                    return false;
-                } 
-                return okay && true;
-            }
-
-            function okayToSubmit(form) {
-                $("#output").empty();
-                var okay = true;
-                okay = lengthTest("Project name", okay, "#ProjectName", 120, -1);
+$(document).ready(function () {
+    $('#removePublicityLink').on('click', function (e) {
+        if(link_count >= 0) {
+            $("#publicity_link" + link_count.toString()).empty();
+            $("#publicity_link"+link_count).remove();
+            link_count--;
+        }
+    });
+});
 
 
-                okay = lengthTest("Email", okay, "#email", 40, -1);
+function lengthTest(type, okay, id, max_length, index) {
+    length = 0;
+    if(index >= 0) {
+        length = $(id + index).val().length;
+    } else {
+        length = $(id).val().length;
+    }
 
-                okay = lengthTest("Project Description", okay, "#ProjectDescription", 1400, -1);
+    if((length > max_length) || (length <= 0)) {
+        $("#output").append(type + " length cannot exceed " + length + " characters<br />");
+        return false;
+    } 
+    return okay && true;
+}
 
-
-                var HSRDL = $("#HSRDescription").val();
-                var HSRRL = $("#HSRRole").val();
-                if(HSRDL === 0 ^ HSRRL === 0) {
-                    $("#output").append("Help Support Request must be fully filled out before submission<br />");
-                    okay = false;
-                } else if(HSRDL > 700) {
-
-                } else if(HSRRL > 40) {
-
-                }
-
-                var FSRDL = $("#FSRDescription").val();
-                var FSRIL = $("#FSRItem").val();
-                var FSRQ = $("FSRQuantity");
-                if(!((FSRDL === 0) ^ (FSRIL === 0) ^ (FSRQ === 0))) {
-                } else if(FSRDL > 700) {
-                } else if(FSRIL > 40) {
-                } else if(!FSRQ.isNaN() || FSRQ <= 0) {
-                }
+function okayToSubmit(form) {
+    $("#output").empty();
+    var okay = true;
+    okay = lengthTest("Project name", okay, "#ProjectName", 120, -1);
 
 
-                var MSRDL = $("#MSRDescription").val();
-                var MSRA = $("#MSRAmount").val();
-                var MSRall_or_nothing = $("#all_or_nothing").val();
-                if(!((MSRDL === 0) ^ (MSRA === 0) ^ (MSRall_or_nothing === 0))) {
-                } else if(MSRDL > 700) {
-                } else if(!MSRA.isNaN() || MSRA <= 0) {
-                } else if(!MSRall_or_nothing.isNaN() || (MSRall_or_nothing != 3 && MSRall_or_nothing != 2)) {
-                }
+    okay = lengthTest("Email", okay, "#email", 40, -1);
 
-                var OSRDL = $("#OSRDescription").val();
-                if(OSRDL > 700) {
-                }
+    okay = lengthTest("Project Description", okay, "#ProjectDescription", 1400, -1);
 
-                for(var i = 0; i < link_count; i++) {
-                    okay = lengthTest("Publicity Link Name", okay, "#PLName", 80, i);
 
-                    okay = lengthTest("Publicity Link URL", okay, "#PLURL", 140, i);
-                }
+    var HSRDL = $("#HSRDescription").val();
+    var HSRRL = $("#HSRRole").val();
+    if(HSRDL === 0 ^ HSRRL === 0) {
+        $("#output").append("Help Support Request must be fully filled out before submission<br />");
+        okay = false;
+    } else if(HSRDL > 700) {
 
-                if(okay) {
-                    alert('hello');
-                    appendToForm();
-                    form.submit();
-                    return true;
-                }
-                else
-                    return false;       
-            }
+    } else if(HSRRL > 40) {
 
-            function appendToForm() {
+    }
 
-                $('<input>').attr({
-                    type: 'hidden',
-                    id: 'link_count',
-                    name: 'link_count',
-                    value: link_count.toString()
-                }).appendTo('form');
+    var FSRDL = $("#FSRDescription").val();
+    var FSRIL = $("#FSRItem").val();
+    var FSRQ = $("FSRQuantity");
+    if(!((FSRDL === 0) ^ (FSRIL === 0) ^ (FSRQ === 0))) {
+    } else if(FSRDL > 700) {
+    } else if(FSRIL > 40) {
+    } else if(!FSRQ.isNaN() || FSRQ <= 0) {
+    }
 
-                $('<input>').attr({
-                    type: 'hidden',
-                    id: 'user_email',
-                    name: 'user_email',
-                    value: user_email
-                }).appendTo('form');
-            }
+
+    var MSRDL = $("#MSRDescription").val();
+    var MSRA = $("#MSRAmount").val();
+    var MSRall_or_nothing = $("#all_or_nothing").val();
+    if(!((MSRDL === 0) ^ (MSRA === 0) ^ (MSRall_or_nothing === 0))) {
+    } else if(MSRDL > 700) {
+    } else if(!MSRA.isNaN() || MSRA <= 0) {
+    } else if(!MSRall_or_nothing.isNaN() || (MSRall_or_nothing != 3 && MSRall_or_nothing != 2)) {
+    }
+
+    var OSRDL = $("#OSRDescription").val();
+    if(OSRDL > 700) {
+    }
+
+    for(var i = 0; i < link_count; i++) {
+        okay = lengthTest("Publicity Link Name", okay, "#PLName", 80, i);
+
+        okay = lengthTest("Publicity Link URL", okay, "#PLURL", 140, i);
+    }
+
+    if(okay) {
+        alert('hello');
+        appendToForm();
+        form.submit();
+        return true;
+    }
+    else
+        return false;       
+}
+
+function appendToForm() {
+
+    $('<input>').attr({
+        type: 'hidden',
+        id: 'link_count',
+        name: 'link_count',
+        value: link_count.toString()
+    }).appendTo('form');
+
+    $('<input>').attr({
+        type: 'hidden',
+        id: 'user_email',
+        name: 'user_email',
+        value: user_email
+    }).appendTo('form');
+}
--- php/includes/create_project.inc.php
+++ php/includes/create_project.inc.php
@@ -28,12 +28,11 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
 
     if ($r) {
         $count = 0;
-        while($user = oci_fetch_row($stmt)) {
+        while($proj = oci_fetch_row($stmt)) {
             $count++;
         }
 
         if ($count > 0) {
-            // A user with this email address already exists
             $error_msg .= '<p class="error">A project with this name already exists.</p>';
         }
     } else {
@@ -43,10 +42,6 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
 
 
     if (empty($error_msg)) {
-        // Insert the new user into the database
-        //$stid = oci_parse($mysqli, 'INSERT INTO Projects (projname, email, description, date_created, user_email) VALUES('.$name.', '.$email.', \"'.$description.'\", TO_DATE('.date("mdY").', MMDDYYYY), '.$user_email.')');
-
-        // $stid = oci_parse($mysqli, 'INSERT INTO Projects (projname, user_email) VALUES('.$name.', '.$user_email.')');
 
         $stid = oci_parse($mysqli, "INSERT INTO Projects (projname, email, description, date_created, user_email) VALUES(:name, :email, :description, TO_DATE('". (string)date("mdY") . "', 'MMDDYYYY'), :user_email)");
 
@@ -66,16 +61,24 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
 
         if(isset($_POST['FSRItem'], $_POST['FSRDescription'], $_POST['FSRQuantity'])) {
             $FSRItem = $_POST['FSRItem'];
-            $FSRDescription = $_POST['FSRDescription'];
+            $FSRDescription = filter_input(INPUT_POST, 'FSRDescription', FILTER_SANITIZE_STRING);;
             $FSRQuantity = $_POST['FSRQuantity'];
-            if($FSRItem != "" && $FSRDescription != "" && FSRQuantity != "") {
+            
+            if($FSRItem != "" && $FSRDescription != "" && $FSRQuantity != "") {
+                
+                $stid = oci_parse($mysqli, "INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES( :FSRDescription, 'food', 0.0, :name )");
+                oci_bind_by_name($stid, ':FSRDescription', $FSRDescription);
+                oci_bind_by_name($stid, ':name', $name);
 
-                $stid = oci_parse($mysqli, 'INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(' . $FSRDescription. ', food, 0, ' . $name .')');
                 $r = oci_execute($stid);  // executes and commits
                 if (!$r) {
                     $error_msg .= '<p class="error">Error inserting Food into database.</p>';
                 }else {
-                    $stid = oci_parse($mysqli, 'INSERT INTO Food_requests (item, quantity, projname, category) VALUES(' . $FSRItem . ', ' . $FSRQuantity . ', ' . $name . ', food)');
+                    $stid = oci_parse($mysqli, "INSERT INTO Food_requests (item, quantity, projname, category) VALUES(:FSRItem, :FSRQuantity , :name, 'food')");
+                    oci_bind_by_name($stid, ':FSRItem', $FSRItem);
+                    oci_bind_by_name($stid, ':FSRQuantity', $FSRQuantity);
+                    oci_bind_by_name($stid, ':name', $name);
+
                     $r = oci_execute($stid);  // executes and commits
                     if (!$r) {
                         $error_msg .= '<p class="error">Error inserting Food into Food table.</p>';
@@ -89,12 +92,18 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
             $HSRDescription = $_POST['HSRDescription'];
 
             if($HSRRole != "" && $HSRDescription != "") {
-                $stid = oci_parse($mysqli, 'INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(' . $HSRDescription. ', help, 0, ' . $name .')');
+                $stid = oci_parse($mysqli, "INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(:HSRDescription, 'help', 0.0, :name)");
+                oci_bind_by_name($stid, ':HSRDescription', $HSRDescription);
+                oci_bind_by_name($stid, ':name', $name);
+
                 $r = oci_execute($stid);  // executes and commits
                 if (!$r) {
                     $error_msg .= '<p class="error">Error inserting Help into database.</p>';
                 }else {
-                    $stid = oci_parse($mysqli, 'INSERT INTO Help_requests (role, projname, category) VALUES(' . $HSRRole . ', ' . $name . ', help)');
+                    $stid = oci_parse($mysqli, "INSERT INTO Help_requests (role, projname, category) VALUES(:HSRRole, :name , 'help')");
+                    oci_bind_by_name($stid, ':HSRRole', $HSRRole);
+                    oci_bind_by_name($stid, ':name', $name);
+
                     $r = oci_execute($stid);  // executes and commits
                     if (!$r) {
                         $error_msg .= '<p class="error">Error inserting Help into Help table.</p>';
@@ -108,16 +117,23 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
             $MSRAmount = $_POST['MSRAmount'];
             $all_or_nothing = $_POST['all_or_nothing'];
 
-            $all_or_nothing_val = 0;
-            if($all_or_nothing == "yes") $all_or_nothing_val = 1; 
+            $all_or_nothing_val = "0";
+            if($all_or_nothing == "yes") $all_or_nothing_val = "1"; 
 
             if($MSRDescription != "" && $MSRAmount != "" && $all_or_nothing != "") {
-                $stid = oci_parse($mysqli, 'INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(' . $MSRDescription. ', money, 0, ' . $name .')');
+                $stid = oci_parse($mysqli, "INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(:MSRDescription, 'money', 0.0, :name)");
+                oci_bind_by_name($stid, ':MSRDescription', $MSRDescription);
+                oci_bind_by_name($stid, ':name', $name);
+
                 $r = oci_execute($stid);  // executes and commits
                 if (!$r) {
                     $error_msg .= '<p class="error">Error inserting Money into database.</p>';
                 }else {
-                    $stid = oci_parse($mysqli, 'INSERT INTO Money_requests (amount, is_all_or_nothing, projname, category) VALUES(' . $MSRDescription . ', ' . $all_or_nothing_val . ', ' . $name . ', money)');
+                    $stid = oci_parse($mysqli, "INSERT INTO Money_requests (amount, is_all_or_nothing, projname, category) VALUES(:MSRAmount , :all_or_nothing_val , :name , 'money')");
+                    oci_bind_by_name($stid, ':MSRAmount', $MSRAmount);
+                    oci_bind_by_name($stid, ':all_or_nothing_val', $all_or_nothing_val);
+                    oci_bind_by_name($stid, ':name', $name);
+
                     $r = oci_execute($stid);  // executes and commits
                     if (!$r) {
                         $error_msg .= '<p class="error">Error inserting Money into Money table.</p>';
@@ -128,18 +144,29 @@ if (isset($_POST['ProjectName'], $_POST['email'], $_POST['ProjectDescription'], 
 
         if(isset($_POST['OSRDescription'])) {
             $OSRDescription = $_POST['OSRDescription'];
-            $stid = oci_parse($mysqli, 'INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(' . $OSRDescription. ', other, 0, ' . $name .')');
-            $r = oci_execute($stid);  // executes and commits
-            if (!$r) {
-                $error_msg .= '<p class="error">Error inserting Other into database.</p>';
+            if($OSRDescription != "") {
+                $stid = oci_parse($mysqli, "INSERT INTO Support_requests (description, category, percent_fulfilled, projname) VALUES(:OSRDescription, 'other', 0.0, :name)");
+                oci_bind_by_name($stid, ':OSRDescription', $OSRDescription);
+                oci_bind_by_name($stid, ':name', $name);
+
+                $r = oci_execute($stid);  // executes and commits
+                if (!$r) {
+                    $error_msg .= '<p class="error">Error inserting Other into database.</p>';
+                }
             }
         }
 
-        for($i = 0; $i < $link_count; $i++) {
-            $PLName = $_POST['PLName' . i];
-            $PLURL = $_POST['PLURL' . i];
+        $error_msg .= 'Link count: ' . $link_count;
+        for($i = 0; $i <= $link_count; $i++) {
+            $PLName = $_POST['PLName' . (string)$i];
+            $PLURL = $_POST['PLURL' . (string)$i];
 
-            $stid = oci_parse($mysqli, 'INSERT INTO Publicity_links (url, website_name, projname) VALUES(' . $PLURL. ', ' . $PLName . ', ' . $name  . ')');
+            $stid = oci_parse($mysqli, 'INSERT INTO Publicity_links (url, website_name, projname) VALUES(:PLURL, :PLName , :name  )');
+            oci_bind_by_name($stid, ':PLURL', $PLURL);
+            oci_bind_by_name($stid, ':PLName', $PLName);
+            oci_bind_by_name($stid, ':name', $name);
+
+
             $r = oci_execute($stid);  // executes and commits
             if (!$r) {
                 $error_msg .= '<p class="error">Error inserting Other into database.</p>';
